07c2acb582ea042668d050f812a51556c1077bcc
illoeliada9-dot <illoeliada9@gmail.com>
2026-08-02 13:51:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 function ouvrirLivre() {
-
-    alert("Bienvenue dans le livre d'anniversaire de Maman ❤️");
-
     const livre = document.getElementById("livre");
-    livre.classList.remove("livre-cache");
     livre.classList.add("livre-ouvert");
-
-    const musique = document.getElementById("musique");
-    musique.play();
 }
 
-    // Ouverture du livre
-    document.getElementById("livre").classList.add("ouvert");
-
-    // Démarrage de la musique
-    const musique = document.getElementById("musique");
-    musique.play();
-}
